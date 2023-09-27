ee6a17a1c8e03de638df3e4f0072677e7b2b72b9
jmartinez0 <jmartinez.16c@gmail.com>
2023-09-26 22:44:14 -0400

    Connected variables in MainActivity.kt to the names in activity_main.xml

--- app/src/main/java/com/example/gpa_juanmartinez_calculator/MainActivity.kt
+++ app/src/main/java/com/example/gpa_juanmartinez_calculator/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.gpa_juanmartinez_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var grade1 : EditText
+    lateinit var grade2 : EditText
+    lateinit var grade3 : EditText
+    lateinit var grade4 : EditText
+    lateinit var grade5 : EditText
+    lateinit var computedGpa : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        grade1 = findViewById(R.id.grade_one)
+        grade2 = findViewById(R.id.grade_two)
+        grade3 = findViewById(R.id.grade_three)
+        grade4 = findViewById(R.id.grade_four)
+        grade5 = findViewById(R.id.grade_five)
+        computedGpa = findViewById(R.id.gpa)
     }
+
+
 }
